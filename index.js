1ea007a8a7b056aa2c5de332fa760611d4a4a59a
Riddhi Gupta <riddhig566@gmail.com>
2024-07-09 22:50:24 -0700

    remove bad import

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const session = require("express-session")
 const dotenv = require("dotenv");
 const cors = require("cors");
 const mongoose = require("mongoose");
